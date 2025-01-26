62b118e46a3a1c22e549ff8d92a148c10d6ff1f9
aprildaghh <abdullah.aslan.se@gmail.com>
2025-01-26 03:23:18 +0300

    gateway

--- Gateway/src/main/java/com/abdullah/Gateway/GatewayApplication.java
+++ Gateway/src/main/java/com/abdullah/Gateway/GatewayApplication.java
@@ -2,12 +2,30 @@ package com.abdullah.Gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/api/v1/comments/**")
+						.uri("http://localhost:8081/"))
+				.route(p -> p
+						.path("/api/v1/appointment/**")
+						.uri("http://localhost:8082/"))
+//				.route(p -> p
+//						.path("/api/v1/4453/**")
+//						.uri("http://localhost:8083/"))
+				.build();
 	}
 
 }
